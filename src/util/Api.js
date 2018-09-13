ec08af818cfdcb3d9adb4fecd5aefbf8aba56587
Joffily Ferreira <joffily@outlook.com>
2018-09-12 22:41:32 -0300

    Change tabs to space

--- src/util/Api.js
+++ src/util/Api.js
@@ -1,15 +1,15 @@
 export const getBusList = (stopObject) => {
 
-	let content = `latitude=${stopObject.LatLng.lat}&longitude=${stopObject.LatLng.lng}&parada=${stopObject.StopName}`;
-	content += '&token=LKM9JuK8rOYCPcg8A6TJHPTcY6pa0B4sfbP3ooarpya6zvJgqOX5RBX60sjNEtHq';
-	content += '&cidade=jpa'
+  let content = `latitude=${stopObject.LatLng.lat}&longitude=${stopObject.LatLng.lng}&parada=${stopObject.StopName}`;
+  content += '&token=LKM9JuK8rOYCPcg8A6TJHPTcY6pa0B4sfbP3ooarpya6zvJgqOX5RBX60sjNEtHq';
+  content += '&cidade=jpa'
 
-	fetch('https://apissl.bus4.com.br/api/localizemev2', {
-	  method: 'POST',
-	  headers: {
-	    'Content-type': 'application/x-www-form-urlencoded'
-	  },
-	  body: content
-	})
+  fetch('https://apissl.bus4.com.br/api/localizemev2', {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/x-www-form-urlencoded'
+    },
+    body: content
+  })
 }
 
