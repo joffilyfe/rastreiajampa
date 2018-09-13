864d147d77169f5203fa745b8ea457d5530dc995
Joffily Ferreira <joffily@outlook.com>
2018-09-12 22:44:53 -0300

    Add API new token generation call

--- src/util/Api.js
+++ src/util/Api.js
@@ -1,3 +1,24 @@
+/**
+ * Get a new token from API.
+ * return {string} token
+ */
+export const getNewToken = async () => {
+
+  let response = await fetch('https://apissl.bus4.com.br/api/dispositivo', {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/x-www-form-urlencoded',
+    },
+    body: 'macaddress=127.0.0.1&cidade=jpa'
+  }).catch(err => {
+    return err;
+  });
+
+  let {Token} = await response.json();
+
+  return Token;
+}
+
 export const getBusList = (stopObject) => {
 
   let content = `latitude=${stopObject.LatLng.lat}&longitude=${stopObject.LatLng.lng}&parada=${stopObject.StopName}`;
